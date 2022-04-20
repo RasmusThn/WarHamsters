c8f8cda828331c3c0b2bd269832ec8dc0a1a9317
Rasmus Larsson <89934634+RasmusThn@users.noreply.github.com>
2022-04-20 16:04:29 +0200

    Laddas fram ny hamster och dem gamla ligger ovanför

--- WarHamsters/Components/HamsterComponent.razor.cs
+++ WarHamsters/Components/HamsterComponent.razor.cs
@@ -7,61 +7,32 @@ namespace WarHamsters.Components;
 
 public partial class HamsterComponent : ComponentBase
 {
-  
+    
+    public Hamster OldHamster1 { get; set; }
+    public Hamster OldHamster2 { get; set; }
     private Hamster hamster1;
     private Hamster hamster2;
     public List<Hamster> Hamsters { get; set; } = new List<Hamster>();
 
-    public async Task AddWin(Hamster hamsterWin, Hamster hamsterLoss)    // Bör ändras till Task<Class> etc
+    public async Task AddWin(Hamster hamsterWin, Hamster hamsterLoss)  
     {
         await hamsterService.AddWin(hamsterWin, hamsterLoss);
-        //ShouldRender();
-
+        
+        OldHamster1 = Hamsters[0];
+        OldHamster2 = Hamsters[1];
+        GetTwoRandomHamsters();
+        StateHasChanged(); //Här laddas det om
     }
-
-    public int ShuffleNumber()
-    {
-        Random random = new Random();
-        int randomNumber = random.Next(1, 6);//ändra till Hamster.count()
-        return randomNumber;
-    }
-    public async Task<Hamster> GetHamsterById(int hamsterId)
-    {
-        return await hamsterService.GetHamsterById(hamsterId);
-    }
+  
     public async void GetTwoRandomHamsters()
     {
         Hamsters = await hamsterService.GetTwoRandomHamsters();
-    }
-
-
-    //Nått med detta...
-
-
-    //protected override bool ShouldRender()
-    //{
-
-    //    return shouldRender;
-    //}
-    protected override async void OnInitialized()
-    {
-        GetTwoRandomHamsters();
         hamster1 = Hamsters[0];
         hamster2 = Hamsters[1];
-
-        //int ham1 = ShuffleNumber();
-        //int ham2 = ShuffleNumber();
-
-        //while (ham1 == ham2)
-        //{
-        //    ham1 = ShuffleNumber();
-        //    ham2 = ShuffleNumber();
-        //}
-
-     
-        //hamster1 = await GetHamsterById(ham1);
-        //hamster2 = await GetHamsterById(ham2);
-
+    }
+    protected override void OnInitialized()
+    {
+        GetTwoRandomHamsters();
     }
 
 }
